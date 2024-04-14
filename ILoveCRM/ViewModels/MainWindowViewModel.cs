e3c40f01319c202347ede398ea36529859fd81f5
Мурат Ханов <Gadgikjaht@mail.ru>
2024-04-14 11:28:05 +0300

    Добавил интерфейс IDisposable в классе модели представления для отработки примера реализации

---
+++ ILoveCRM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ILoveCRM.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILoveCRM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
