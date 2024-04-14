f54a07c69aeda8e88c266bc6b8ced8d4cd5bb8e7
Мурат Ханов <Gadgikjaht@mail.ru>
2024-04-14 12:20:44 +0300

    Тестирование работы модели-преставления главного окна

--- ILoveCRM/ViewModels/MainWindowViewModel.cs
+++ ILoveCRM/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,26 @@ namespace ILoveCRM.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "ILoveCRM";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged;
+            //}
+
+            //Set(ref _Title, value);
+
+            set => Set(ref _Title, value);
+        }
+        #endregion
 
     }
 }
